5303a0d2d56a9376391a3a89d69855408280b519
jbmasemza <jbmasemza@yahoo.com>
2020-02-21 10:51:53 +0200

    Add files via upload

--- person.py
+++ person.py
@@ -1,14 +1,15 @@
 class person:
 
-    def __init__(self,name,age,interest):
+    def __init__(self,name,age,gender,interest):
         self.name = name
         self.age = age
+        self.gender = gender
         self.interest = ''.join(interest) 
 
     def hellow(self):
         return f'Hellow my name is {self.name} and I am {self.age}. My interest are {self.interest}'
 
-person = person('Ryan',30,['being a hardarse ',' agile ', ' ssd hard drives '])
+person = person('Ryan',30,'male',['being a hardarse, ',' agile  and ', ' ssd hard drives '])
 greeting = person.hellow()
 
 print(greeting)
